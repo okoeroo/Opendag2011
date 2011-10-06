2b3fee31e4f35c3b6a91ec1ed21fa74083dfed16
Oscar Koeroo <okoeroo@gmail.com>
2011-10-06 13:44:41 +0200

    Fixed vertical mode

--- run_rsync.sh
+++ run_rsync.sh
@@ -8,15 +8,21 @@ SOURCE="."
 DESTHOST="stropdassen.haatik.nl"
 DESTPATH="/home/www-data/oscar/nikhefwebapp/"
 
+SOURCE="/Users/okoeroo/dvl/webapps/opendag2011/www/."
 DESTHOST="login2.nikhef.nl"
 DESTPATH="/www/od11/"
 
-rsync --verbose --progress --stats \
-      -a \
+
+scp -r \
+      $SOURCE $DESTHOST:$DESTPATH
+
+exit $?
+
+
+rsync --partial --verbose --progress --stats \
       -S \
       -z \
       -e ssh \
       --exclude ".git" \
-      --exclude ".*swp" \
       $SOURCE $DESTHOST:$DESTPATH
 
